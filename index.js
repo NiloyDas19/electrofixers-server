4b35de6a34dae6cdc2c46da3bc68de0904ab0780
Niloy Das <niloydas5215@gmail.com>
2024-05-14 04:32:02 +0600

    services get and post implemented

--- index.js
+++ index.js
@@ -20,41 +20,53 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 
 async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    const servicesCollection = client.db("servicesDB").collection('services');
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        const servicesCollection = client.db("servicesDB").collection('services');
 
-    
-    
+        app.get('/services', async (req, res) => {
+            const cursor = servicesCollection.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+
+        app.post('/services', async (req, res) => {
+            const newService = req.body;
+            console.log(newService);
+            const result = await servicesCollection.insertOne(newService);
+            res.send(result);
+        })
 
 
-    // Send a ping to confirm a successful connection
-    // await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
-  }
+
+
+        // Send a ping to confirm a successful connection
+        // await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
 }
 run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-  res.send('elctrofixers server is running...');
+    res.send('elctrofixers server is running...');
 })
 
 
 app.listen(port, () => {
-  console.log(`elctrofixers server is running in the ${port} port`);
+    console.log(`elctrofixers server is running in the ${port} port`);
 })
 
